df8900f69490c3b2940cf33663e19e73dc52098d
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-07-06 16:30:05 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,86 +1,23 @@
 import React from "react";
 
-export default function Die(props: any) {
-  const styles = {
-    backgroundColor: props.isHeld ? "rgba(247, 182, 41, 0.9)" : "white",
-  };
 
-  function face() {
-    // eslint-disable-next-line default-case
-    switch (props.value) {
-      case 1:
-        return (
-          <div className="dice first-face" style={styles}>
-            <span className="dot"></span>
-          </div>
-        );
-      case 2:
-        return (
-          <div className="dice second-face" style={styles}>
-            <span className="dot"></span>
-            <span className="dot"></span>
-          </div>
-        );
-      case 3:
-        return (
-          <div className="dice third-face" style={styles}>
-            <span className="dot"></span>
-            <span className="dot"></span>
-            <span className="dot"></span>
-          </div>
-        );
-      case 4:
-        return (
-          <div className="fourth-face dice" style={styles}>
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-          </div>
-        );
-      case 5:
-        return (
-          <div className="fifth-face dice" style={styles}>
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
 
-            <div className="column">
-              <span className="dot"></span>
-            </div>
 
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-          </div>
-        );
-      case 6:
-        return (
-          <div className="sixth-face dice" style={styles}>
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="column">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-          </div>
-        );
-    }
-  }
 
+ const Footer: React.FC = () => {
   return (
-    <div className="die-face" onClick={props.holdDice}>
-      {face()}
-    </div>
+    <footer>
+      <p>
+        <a
+          href="https://github.com/michaelkolesidis/tenzi"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <span id="made">Made with </span><span id="heart">♥</span> <br/><span id="name">by Michael Kolesidis</span>
+        </a>
+      </p>
+    </footer>
   );
 }
+
+export default Footer;
